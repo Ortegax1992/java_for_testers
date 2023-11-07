b420036aa6097334f44767fc496e7233af862806
Ortegax28 <minin_kos@mail.ru>
2023-11-08 01:37:24 +0300

    created triangle class and add tests for it

---
+++ sandbox/src/main/java/lesson1/geometry/Geometry.java
@@ -0,0 +1,9 @@
+package lesson1.geometry;
+
+import lesson1.geometry.figures.Triangle;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Triangle.printTriangleArea(new Triangle(3, 3, 4));
+    }
+}
---
+++ sandbox/src/test/java/geometry/figures/TriangleTests.java
@@ -0,0 +1,30 @@
+package geometry.figures;
+
+import lesson1.geometry.figures.Triangle;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class TriangleTests {
+
+    @Test
+    void calculateArea() {
+        var t = new Triangle(3, 3, 4);
+        MathContext context = new MathContext(4, RoundingMode.HALF_UP);
+        BigDecimal result = new BigDecimal(t.area(), context);
+        Assertions.assertEquals(4.472, result.doubleValue());
+    }
+
+    @Test
+    void calculatePerimeter() {
+        Assertions.assertEquals(16, new Triangle().perimeter(4, 5, 7));
+    }
+
+    @Test
+    void calculateSemierimeter(){
+        Assertions.assertEquals(12,new Triangle().semiperimeter(6,8,10));
+    }
+}
