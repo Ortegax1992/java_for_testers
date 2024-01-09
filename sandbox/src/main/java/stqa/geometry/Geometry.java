0a51ac659924d7c0d3bed0e74909f0806e49db74
Ortegax28 <minin_kos@mail.ru>
2024-01-10 01:34:40 +0300

    эксперименты с лямбда выражениями для геометрических фигур, переделал под лямбду выбор всех групп

--- sandbox/src/main/java/stqa/geometry/Geometry.java
+++ sandbox/src/main/java/stqa/geometry/Geometry.java
@@ -2,8 +2,15 @@ package stqa.geometry;
 
 import stqa.geometry.figures.Triangle;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class Geometry {
     public static void main(String[] args) {
-        Triangle.printTriangleArea(new Triangle(8, 9, 12));
+        var triangles = List.of(new Triangle(8, 9, 12), new Triangle(7, 8, 11), new Triangle(6, 7, 10));
+//        for (Triangle triangle : triangles){
+//            Triangle.printTriangleArea(triangle);
+//        }
+        triangles.forEach(Triangle::printTriangleArea);
     }
 }
