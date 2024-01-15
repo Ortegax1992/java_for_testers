befcd50316965d5d47ca52e3f5670362565306ff
Ortegax28 <minin_kos@mail.ru>
2024-01-15 22:16:01 +0300

    тест на возможность создания юзера из лекции

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/UserCreationTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserCreationTests.java
@@ -7,7 +7,6 @@ import ru.stqa.mantis.common.CommonFunctions;
 import ru.stqa.mantis.model.DeveloperMailUser;
 
 import java.time.Duration;
-import java.util.regex.Pattern;
 
 public class UserCreationTests extends TestBase {
 
@@ -18,28 +17,22 @@ public class UserCreationTests extends TestBase {
         String password = "password";
         user = app.developerMail().addUser();
         String email = String.format("%s@developermail.com", user.name());
-//        app.jamesApi().addUser(email, password);
+        app.jamesApi().addUser(email, password);
 
 
+        app.session().registration(user.name(), email);
 
-//        app.session().registration(user, email);
-//        var messages = app.mail().receive(email, password, Duration.ofSeconds(10));
-//        var text = messages.get(0).content();
-//        var pattern = Pattern.compile("http://\\S*");
-//        var matcher = pattern.matcher(text);
-//        if (matcher.find()) {
-//            var url = text.substring(matcher.start(), matcher.end());
-//            System.out.println(url);
-//            app.driver().get(url);
-//        }
-//        var name = "name";
-//        app.session().followTheLinkAndRegister(name, password);
-//        app.http().login(user, password);
-//        Assertions.assertTrue(app.http().isLoggedIn());
+        var messages = app.developerMail().receive(user, Duration.ofSeconds(10));
+        var url = CommonFunctions.extractUrl(messages);
+
+        app.session().followTheLinkAndRegister(url, user.name(), password);
+
+        app.http().login(user.name(), password);
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
 
     @AfterEach
-    void deleteMailUser(){
+    void deleteMailUser() {
         app.developerMail().deleteUser(user);
     }
 }
